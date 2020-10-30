8cf62c14897f16fc9f4122212449ed8210d5731d
Bartlomiej.Kwiatek <bartlomiej.kwiatek@vml.com>
2020-10-30 11:46:48 +0100

    covid - always download new file

--- CSSEGISandData-COVID-19-download.sh
+++ CSSEGISandData-COVID-19-download.sh
@@ -5,27 +5,18 @@ cd $(dirname $0)
 download_file() {
     local remote_file_path="https://raw.githubusercontent.com/CSSEGISandData/COVID-19/$1"
     local local_file_path=$2
-    local date_format=$3
-    current_date=$(date +${date_format})
-    if [[ "Darwin" == "$(uname)" ]]; then
-        file_date=$(stat -f "%Sm" -t "${date_format}" "$local_file_path")
-    else
-        file_date=$(date -d @$(stat -c "%Y" "$local_file_path") +${date_format})
-    fi
-    if [[ "$file_date" -lt "$current_date" ]]; then
-        printf "Download $remote_file_path"
-        curl -sL $remote_file_path >$local_file_path
-        printf " [OK]\n"
-    fi
+    printf "Download $remote_file_path"
+    curl -sL -o "$local_file_path" -z "$local_file_path" "$remote_file_path"
+    printf " [OK]\n"
 }
 
 series=(confirmed recovered deaths)
 for serie in "${series[@]}"; do
-    download_file "master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_${serie}_global.csv" "data/time_series_${serie}_global.csv" "%Y%m%d"
+    download_file "master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_${serie}_global.csv" "data/time_series_${serie}_global.csv"
 done
-download_file "master/csse_covid_19_data/UID_ISO_FIPS_LookUp_Table.csv" "data/UID_ISO_FIPS_LookUp_Table.csv" "%Y%m%d%H"
-#download_file "master/csse_covid_19_data/csse_covid_19_daily_reports/$(date -v-1d +%m-%d-%Y).csv" "data/daily_reports.csv" "%Y%m%d%H"
-download_file "web-data/data/cases_country.csv" "data/cases_country.csv" "%Y%m%d%H"
+download_file "master/csse_covid_19_data/UID_ISO_FIPS_LookUp_Table.csv" "data/UID_ISO_FIPS_LookUp_Table.csv"
+#download_file "master/csse_covid_19_data/csse_covid_19_daily_reports/$(date -v-1d +%m-%d-%Y).csv" "data/daily_reports.csv"
+download_file "web-data/data/cases_country.csv" "data/cases_country.csv"
 
 if [[ "Darwin" == "$(uname)" ]]; then
     php CSSEGISandData-COVID-19-parse.php
